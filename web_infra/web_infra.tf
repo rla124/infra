a12711157c2f33e1e8674a331eb08ceba00fdddf
Subin <rla0010101@gmail.com>
2024-03-28 23:18:31 +0900

    ssh 접속 허용 인바운드 규칙 시큐리티 그룹 설정 후 terraform plan, apply

--- web_infra/web_infra.tf
+++ web_infra/web_infra.tf
@@ -2,3 +2,14 @@ resource "aws_key_pair" "web_admin" {
   key_name = "web_admin"
   public_key = file("~/.ssh/web_admin.pub")
 }
+
+resource "aws_security_group" "ssh" {
+  name = "allow_ssh_from_all"
+  description = "Allow SSH port from all"
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
